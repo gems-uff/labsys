b0bdcffbfeb6fbb437dec0aab5c264652826993a
Gabriel Saldanha <gabrielcrsaldanha@gmail.com>
2017-04-23 12:29:43 -0300

    :art: Convert our index, detail and results into Generic Views
    Finish tutorial part 4.

--- polls/views.py
+++ polls/views.py
@@ -1,10 +1,12 @@
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
 from django.urls import reverse
+from django.views import generic
 
 from .models import Question, Choice
 
-# Create your views here.
+'''
+FBV Views
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
     context = {
@@ -19,6 +21,32 @@ def detail(request, question_id):
 def results(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     return render(request, 'polls/results.html', {'question': question})
+
+'''
+
+
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    """
+    The default context variable is <question>_list, (queryset)
+    so we override it providing context_object_name
+    """
+    context_object_name = 'latest_question_list'
+
+    def get_queryset(self):
+        """Return the last five published questions."""
+        return Question.objects.order_by('-pub_date')[:5]
+
+
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'polls/detail.html'
+
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
+
 
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
