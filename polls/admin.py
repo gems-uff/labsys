e91051acf7f8801b99d3065c9aaa676c2893f6ad
Gabriel Saldanha <gabrielcrsaldanha@gmail.com>
2017-04-23 14:30:58 -0300

    :rocket Part 6: modify admin change page
    Add custom attributes to be displayed

--- polls/admin.py
+++ polls/admin.py
@@ -14,5 +14,7 @@ class QuestionAdmin(admin.ModelAdmin):
         ('Date information', {'fields': ['pub_date']}),
     ]
     inlines = [ChoiceInline]
+    list_display = ('question_text', 'pub_date', 'was_published_recently')
+
 
 admin.site.register(Question, QuestionAdmin)
