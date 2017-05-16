7773afb25d55332e2a53f045ceab6432d2fbfd4b
Gabriel Saldanha <gabrielcrsaldanha@gmail.com>
2017-05-16 11:56:42 -0300

    :memo: Change prod settings

--- labsys/settings/prod.py
+++ labsys/settings/prod.py
@@ -8,7 +8,9 @@ DEBUG=False
 #ALLOWED_HOSTS = ['labsys.herokuapp.com']
 ALLOWED_HOSTS = ['*']
 
-DATABASES['default'] = dj_database_url.config()
+# Update database configuration with $DATABASE_URL.
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
 
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
