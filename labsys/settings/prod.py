57a0aea89060f87b7cc7e59be3ad5bcaa23907b1
Gabriel Saldanha <gabrielcrsaldanha@gmail.com>
2017-05-16 10:06:34 -0300

    :memo: More configurations following:
    http://www.marinamele.com/2013/12/how-to-set-django-app-on-heroku-part-i.html

--- labsys/settings/prod.py
+++ labsys/settings/prod.py
@@ -1,14 +1,18 @@
+import dj_database_url
+
 from .base import *
 
 
 # TODO: Serve files via a dedicated static server then change this value
 DEBUG=False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['labsys.herokuapp.com']
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'fiocruz_prod'
-    }
-}
+DATABASES['default'] = dj_database_url.config()
+
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+STATIC_ROOT = 'staticfiles'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static')
+)
