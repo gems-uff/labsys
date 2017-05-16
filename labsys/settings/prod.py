296d4f50ef8ddd0b0937ba96bcd139d20cd9c568
Gabriel Saldanha <gabrielcrsaldanha@gmail.com>
2017-05-16 10:28:58 -0300

    :memo: One more try...

--- labsys/settings/prod.py
+++ labsys/settings/prod.py
@@ -7,4 +7,11 @@ ALLOWED_HOSTS = ['labsys.herokuapp.com']
 
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgresql-closed-68220'
+    }
+}
+
 STATIC_ROOT = 'staticfiles'
