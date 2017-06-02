037a09a9ea3fe75e800869e3fc311d784b2d3c3f
Gabriel Saldanha <gabrielcrsaldanha@gmail.com>
2017-06-02 11:36:07 -0300

    :memo: Rename requestor to requester

--- admission_notes/models.py
+++ admission_notes/models.py
@@ -1,10 +1,12 @@
 import datetime
 
+#from labsys.patients import Patient
+
 from django.db import models
 
 class AdmissionNote(models.Model):
     id_gal = models.CharField(max_length=30)
-    requestor = models.CharField(max_length=255)
+    requester = models.CharField(max_length=255)
     health_unit = models.CharField(max_length=255)
     state = models.CharField(max_length=2)
     city = models.CharField(max_length=255)
