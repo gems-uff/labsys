676ed74155ccf98adcc19fd37a2cc31e28a99be6
Gabriel Saldanha <gabrielcrsaldanha@gmail.com>
2017-06-03 16:36:48 -0300

    :rocket: Add admin panel for Patient Also remove routes for patient

--- patients/admin.py
+++ patients/admin.py
@@ -1,5 +1,19 @@
 from django.contrib import admin
 
-from .models import Patient
+from .models import Patient, Locality
 
-# Register your models here.
+class PatientAdmin(admin.ModelAdmin):
+    fieldsets = [
+        ('Dados do paciente', {'fields': [
+            'name',
+            'birth_date',
+            'age_in_hours',
+            'gender',
+            'pregnant',
+        ]}),
+        ('Endereço de residência', {'fields': ['residence']}),
+    ]
+
+
+admin.site.register(Locality)
+admin.site.register(Patient, PatientAdmin)
--- patients/views.py
+++ patients/views.py
@@ -1,3 +1,4 @@
 from django.shortcuts import render
+from django.views import generic
 
-# Create your views here.
+from .models import Patient
