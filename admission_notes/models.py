6f0d09ff5f81518daf30b00311ce4ac052e08c14
Gabriel Saldanha <gabrielcrsaldanha@gmail.com>
2017-06-03 15:57:12 -0300

    :art: Rename fields and remove verbose_name attr (1st arg is it by default)

--- admission_notes/models.py
+++ admission_notes/models.py
@@ -4,21 +4,27 @@ import datetime
 
 from django.db import models
 
+
 class AdmissionNote(models.Model):
-    id_gal = models.CharField(max_length=30)
+    id_gal = models.CharField(
+        'Número da requisição (GAL interno)',
+        max_length=30
+    )
     requester = models.CharField(
+        'Instituto solicitante',
         max_length=255,
-        help_text="LACEN ou instituto que solicitou o exame",
+        help_text='LACEN ou instituto que solicitou o exame',
     )
     health_unit = models.CharField(
+        'Unidade de saúde',
         max_length=255,
-        help_text="unidade onde o paciente foi avaliado",
+        help_text='unidade onde o paciente foi avaliado',
     )
     state = models.CharField(max_length=2)
     city = models.CharField(max_length=255)
     admission_date = models.DateField(
-        verbose_name="Data de entrada (LVRS)",
-        help_text="quando a amostra chegou no LVRS",
+        'Data de entrada (LVRS)',
+        help_text='quando a amostra chegou no LVRS',
         null=True,
         blank=True,
     )
