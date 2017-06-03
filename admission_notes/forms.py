ea8b6c1d806a1f42529f47eaa0740b363e63a7df
Gabriel Saldanha <gabrielcrsaldanha@gmail.com>
2017-06-03 11:51:28 -0300

    :rocket: Fix messages :memo: Add commentas about crispy forms

--- admission_notes/forms.py
+++ admission_notes/forms.py
@@ -16,15 +16,21 @@ class AdmissionNoteForm(forms.ModelForm):
 
     def __init__(self, *args, **kwargs):
         super(AdmissionNoteForm, self).__init__(*args, **kwargs)
+        # Builds automacally using default
+        #self.helper = FormHelper(self)
+        # If you want to manipulate some bits of a big layout,
+        # use dynamic layouts (Docs)
+        # FormHelper attributes:
+        # http://django-crispy-forms.readthedocs.io/en/latest/form_helper.html
         self.helper = FormHelper()
+        # self.helper.form_action = 'admission_notes:create'
+        #self.helper.form_method = 'post'
+        # action set to reverse('<form_action>')
+        self.helper.form_tag = False
         self.helper.form_id = 'id-admissionNoteForm'
         self.helper.form_class = 'form-horizontal'
         self.helper.label_class= 'col-lg-2'
         self.helper.field_class = 'col-lg-8'
-        #self.helper.form_method = 'post'
-        # action set to reverse('<form_action>')
-        self.helper.form_action = 'admission_notes:create'
-        self.helper.form_tag = False
         self.helper.add_input(Submit('submit', 'Enviar'))
 
         self.fields['admission_date'].input_formats = DATE_INPUT_FORMATS
