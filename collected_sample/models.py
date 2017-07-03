cb16ba3e7543d1635c5a7e6616a2641b3888c7fc
Gabriel Saldanha <gabrielcrsaldanha@gmail.com>
2017-07-03 15:04:49 -0300

    :rocket: Create model for RTPCR_CDC

--- collected_sample/models.py
+++ collected_sample/models.py
@@ -30,7 +30,7 @@ class CollectedSample(models.Model):
     )
     details = models.CharField(
         'Informações adicionais',
-        max_length=255,
+        max_length=500,
         blank=True,
     )
     admission_note = models.ForeignKey(
@@ -39,3 +39,38 @@ class CollectedSample(models.Model):
 
     def __str__(self):
         return "Amostra coletada em {}".format(self.collection_date)
+
+
+class RTPCR_CDC(models.Model):
+    sample = models.ForeignKey(CollectedSample)
+
+    flu_type = models.IntegerField(
+        verbose_name='Resultado Tipagem',
+        choices=(
+            (1, 'A - Flu A'),
+            (2, 'B - Flu B'),
+            (3, 'Não realizado'),
+            (4, 'Inconclusivo'),
+            (9, 'Ignorado'),
+        ),
+        default=9,
+    )
+    flu_subtype = models.IntegerField(
+        verbose_name='Subtipo ou Linhagem',
+        choices=(
+            (1, 'H1'),
+            (2, 'H3'),
+            (3, 'Yamagata'),
+            (4, 'Victoria'),
+            (5, 'Não realizado'),
+            (6, 'Não subtipável'),
+            (9, 'Ignorado'),
+        ),
+        default=9,
+    )
+
+    details = models.CharField(
+        max_length=255,
+        blank=True,
+    )
+
