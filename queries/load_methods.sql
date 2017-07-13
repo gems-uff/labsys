1fea2890caac146188ea7b3dedd0f4f887e0935e
Gabriel Saldanha <gabrielcrsaldanha@gmail.com>
2017-07-13 14:22:24 -0300

    :rocket: Add load sql files

---
+++ queries/load_methods.sql
@@ -0,0 +1,6 @@
+INSERT INTO public.methods(id, name, "primary") VALUES
+  (1, 'Swab Triplo', TRUE),
+	(2, 'Secreção de oro e nasofaringe', TRUE),
+	(3, 'Tecido post-mortem', TRUE),
+	(4, 'Lavado Bronco-alveolar', TRUE),
+	(5, 'Outro, especifique', TRUE);
