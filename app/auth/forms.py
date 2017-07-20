571308200eb7f90a2a315a34689ccf9e7abdd8df
Gabriel Saldanha <gabrielcrsaldanha@gmail.com>
2017-07-20 17:24:15 -0300

    :rocket: Add User Registration route

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -1,13 +1,38 @@
 from flask_wtf import Form
-from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import InputRequired, Length, Email
+from wtforms import (
+    StringField, PasswordField, BooleanField, SubmitField,
+    ValidationError,
+)
+from wtforms.validators import (
+    InputRequired, Length, Email, Regexp, EqualTo,
+)
+
+from app.models import User
 
 
 class LoginForm(Form):
     email = StringField('Email', validators=[
         InputRequired(), Length(1, 64), Email()])
-    password = PasswordField('Password', validators=[InputRequired()])
-    remember_me = BooleanField('Keep me logged in')
+    password = PasswordField('Senha', validators=[InputRequired()])
+    remember_me = BooleanField('Lembrar')
     submit = SubmitField('Log In')
 
-    
+
+class RegistrationForm(Form):
+    email = StringField('Email', validators=[
+        InputRequired(), Length(1, 64), Email()])
+    username = StringField('Username', validators=[
+        InputRequired(), Length(1, 64)])
+    password = PasswordField('Senha', validators=[
+        InputRequired(), EqualTo('password2',
+                                 message='Senhas devem ser iguais')])
+    password2 = PasswordField('Confirmar senha', validators=[InputRequired()])
+    submit = SubmitField('Registrar')
+
+    def validate_email(self, field):
+        if User.query.filter_by(email=field.data).first():
+            raise ValidationError('Esse email já está em uso!')
+
+    def validate_username(self, field):
+        if User.query.filter_by(username=field.data).first():
+            raise ValidationError('Esse usuário já está em uso!')
--- app/auth/views.py
+++ app/auth/views.py
@@ -1,9 +1,10 @@
 from flask import render_template, redirect, request, url_for, flash
 from flask_login import login_user, logout_user, login_required
 
-from app.models import User
+from ..models import User
+from .. import db
 from . import auth
-from .forms import LoginForm
+from .forms import LoginForm, RegistrationForm
 
 @auth.route('/login', methods=['GET', 'POST'])
 def login():
@@ -13,7 +14,7 @@ def login():
         if user is not None and user.verify_password(form.password.data):
             login_user(user, form.remember_me.data)
             return redirect(request.args.get('next') or url_for('main.index'))
-        flash('Invalid username of password.')
+        flash('Usuário ou password inválido(s).')
 
     return render_template('auth/login.html', form=form)
 
@@ -22,5 +23,18 @@ def login():
 @login_required
 def logout():
     logout_user()
-    flash('You have been logged out.')
+    flash('Log Out realizado com sucesso.')
     return redirect(url_for('main.index'))
+
+
+@auth.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(email=form.email.data,
+                    username=form.username.data,
+                    password=form.password.data)
+        db.session.add(user)
+        flash('Agora você pode realizar o Log In.')
+        return redirect(url_for('auth.login'))
+    return render_template('auth/register.html', form=form)
