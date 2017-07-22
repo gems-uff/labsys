bd53fe8df0642fd85d655994619fedaf4347e826
Gabriel Saldanha <gabrielcrsaldanha@gmail.com>
2017-07-22 11:04:51 -0300

    :rocket: Test create admission (database) with collection date and admission date

--- tests/test_views.py
+++ tests/test_views.py
@@ -1,4 +1,5 @@
 import unittest
+import datetime
 from flask import current_app, url_for, get_flashed_messages
 from app import create_app, db
 from app.models import *
@@ -12,7 +13,9 @@ class TestCreateAdmissionView(unittest.TestCase):
         db.create_all()
         self.client = self.app.test_client(use_cookies=True)
         self.valid_admission_form = {
-            'id_lvrs_intern': 1,
+            'id_lvrs_intern': '011/2012',
+            'samples-0-collection_date': '12/12/2012',
+            'samples-0-admission_date': '13/12/2012',
         }
 
     def tearDown(self):
@@ -21,18 +24,26 @@ class TestCreateAdmissionView(unittest.TestCase):
         self.app_context.pop()
 
     def test_duplicate_id_lvrs_intern(self):
-        a = Admission(id_lvrs_intern=1)
+        data = self.valid_admission_form
+        a = Admission(id_lvrs_intern=data['id_lvrs_intern'])
         db.session.add(a)
         db.session.commit()
-        data = {
-            'id_lvrs_intern': '1',
-            'samples-0-collection_date': '12/12/2012',
-            'samples-0-admission_date': '13/12/2012',
-        }
         duplicate_id_lvrs_intern = 'Número Interno já cadastrado!'
         response = self.client.post(
             url_for('main.create_admission'), data=data, follow_redirects=True)
         self.assertTrue(
             duplicate_id_lvrs_intern in response.get_data(as_text=True))
-        self.assertTrue(
-            len(Admission.query.filter_by(id_lvrs_intern='1').all()), 1)
+        self.assertEqual(len(Admission.query.all()), 1)
+
+    def test_create_valid_admission(self):
+        data = self.valid_admission_form
+        self.assertEqual(len(Admission.query.all()), 0)
+        response = self.client.post(
+            url_for('main.create_admission'), data=data, follow_redirects=True)
+        self.assertTrue(response.status_code == 200)
+        admission = Admission.query.first()
+        self.assertEqual(admission.id_lvrs_intern, data['id_lvrs_intern'])
+        self.assertEqual(admission.samples.first().collection_date,
+                         datetime.date(2012, 12, 12))
+        self.assertEqual(admission.samples.first().admission_date,
+                         datetime.date(2012, 12, 13))
