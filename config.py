793a099010917039f1588bff5237b8d5be186399
Gabriel Saldanha <gabrielcrsaldanha@gmail.com>
2017-07-24 10:20:29 -0300

    :hammer: Add tests for list_admissions and change testing database URL

--- config.py
+++ config.py
@@ -30,7 +30,7 @@ class DevelopmentConfig(Config):
 
 class TestingConfig(Config):
     TESTING = True
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+    SQLALCHEMY_DATABASE_URI = os.environ.get('TEST_DATABASE_URL') or \
         'sqlite:///' + os.path.join(basedir, 'data-test.sqlite')
     WTF_CSRF_ENABLED = False
 
