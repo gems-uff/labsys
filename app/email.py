c513f39f531c54d2ece156738f4a5329bfa1a0bf
Gabriel Saldanha <gabrielcrsaldanha@gmail.com>
2017-08-14 16:44:49 -0300

    :rocket: :bug: Fix email sending - Was not able to send email with special characters

--- app/email.py
+++ app/email.py
@@ -12,9 +12,10 @@ def send_async_email(app, msg):
 def send_email(recipients, subject, template, **kwargs):
     app = current_app._get_current_object()
     msg = Message(
-        app.config['LABSYS_MAIL_SUBJECT_PREFIX'] + ' ' + subject,
+        subject=app.config['LABSYS_MAIL_SUBJECT_PREFIX'] + ' ' + subject,
         sender=app.config['LABSYS_MAIL_SENDER'],
-        recipients=recipients)
+        recipients=recipients,
+        charset='utf-8')
     msg.body = render_template(template + '.txt', **kwargs)
     msg.html = render_template(template + '.html', **kwargs)
     thr = Thread(target=send_async_email, args=[app, msg])
