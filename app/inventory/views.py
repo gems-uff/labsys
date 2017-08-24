90c56e6088e1b3dea01e45c2c8c4930248f571cc
Gabriel Saldanha <gabrielcrsaldanha@gmail.com>
2017-08-24 13:19:17 -0300

    :art: Refactor: move create_add_transaction logic to Transaction

--- app/inventory/views.py
+++ app/inventory/views.py
@@ -64,35 +64,7 @@ def create_add_transaction():
     if form.validate_on_submit():
         transaction = Transaction(user=current_user)
         form.populate_obj(transaction)
-        catalog_product = Product.query.get(transaction.product_id)
-        # Unitary product
-        if catalog_product.is_unitary:
-            transaction.stock_product = StockProduct.query.filter_by(
-                product_id=catalog_product.id,
-                lot_number=form.lot_number.data).first()
-            if transaction.stock_product is None:
-                transaction.stock_product = StockProduct(
-                    product_id=catalog_product.id,
-                    lot_number=form.lot_number.data,
-                    expiration_date=form.expiration_date.data,
-                    amount=transaction.amount, )
-            else:
-                transaction.stock_product.amount += transaction.amount
-        # Non-Unitary => convert parent to child
-        else:
-            product_id = catalog_product.subproduct.id
-            transaction.stock_product = StockProduct.query.filter_by(
-                product_id=product_id,
-                lot_number=form.lot_number.data).first()
-            if transaction.stock_product is None:
-                transaction.stock_product = StockProduct(
-                    product_id=product_id,
-                    lot_number=form.lot_number.data,
-                    expiration_date=form.expiration_date.data,
-                    amount=(transaction.amount * catalog_product.stock_unit))
-            else:
-                transaction.stock_product.amount += (
-                    transaction.amount * catalog_product.stock_unit)
+        transaction.execute(form.data)
         db.session.add(transaction)
         db.session.commit()
         flash('Entrada realizada com sucesso.')
@@ -138,3 +110,10 @@ def create_sub_transaction():
 
     return render_template(
         'inventory/create-transaction.html', form=form, method='sub')
+
+
+@inventory.route('/transactions/add/<int:id>/edit', methods=['GET', 'POST'])
+@login_required
+@permission_required(Permission.EDIT)
+def edit_add_transaction(id):
+    return ('Not implemented yet')
--- app/models.py
+++ app/models.py
@@ -491,8 +491,35 @@ class Transaction(db.Model):
     def get_transactions_ordered(cls):
         return cls.query.order_by(desc(cls.transaction_date)).all()
 
+    def get_stock_product_amount(self, catalog_product, amount):
+        if catalog_product.is_unitary:
+            return amount
+        else:
+            return catalog_product.stock_unit * amount
+
+    def get_stock_product_catalog_id(self, catalog_product):
+        if catalog_product.is_unitary:
+            return catalog_product.id
+        else:
+            return catalog_product.subproduct.id
+
+    def execute(self, form_dict):
+        form_dict = dict((k, v) for k, v in form_dict.items())
+        product = Product.query.get(self.product_id)
+        stock_product = StockProduct.query.filter_by(
+            product_id=product.id, lot_number=form_dict['lot_number']).first()
+        if stock_product is None:
+            stock_product = StockProduct(amount=0)
+            db.session.add(stock_product)
+
+        stock_product.lot_number = form_dict['lot_number']
+        stock_product.amount += self.get_stock_product_amount(
+            product, form_dict['amount'])
+        stock_product.product_id = self.get_stock_product_catalog_id(product)
+        stock_product.expiration_date = form_dict['expiration_date']
+
     def __repr__(self):
-        return '{} : {}'.format(self.transaction_date, self.product.name[:10])
+        return '{} : {}'.format(self.id, self.transaction_date)
 
 
 class StockProduct(db.Model):
