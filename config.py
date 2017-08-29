14a77f5a50ea628936aea3379d25b7167ef23223
Gabriel Saldanha <gabrielcrsaldanha@gmail.com>
2017-08-29 11:45:30 -0300

    :memo: Remove  clause when  is not specified.

--- config.py
+++ config.py
@@ -27,22 +27,19 @@ class Config:
 class DevelopmentConfig(Config):
     DEBUG = True
     BOOTSTRAP_SERVE_LOCAL = True
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DEV_DATABASE_URL') or \
-        'sqlite:///' + os.path.join(basedir, 'data-dev.sqlite')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DEV_DATABASE_URL')
 
 
 class TestingConfig(Config):
     TESTING = True
     BOOTSTRAP_SERVE_LOCAL = True
-    SQLALCHEMY_DATABASE_URI = os.environ.get('TEST_DATABASE_URL') or \
-        'sqlite:///' + os.path.join(basedir, 'data-test.sqlite')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('TEST_DATABASE_URL')
     WTF_CSRF_ENABLED = False
 
 
 class ProductionConfig(Config):
     BOOTSTRAP_SERVE_LOCAL = True
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
-        'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
 
 
 class HerokuConfig(ProductionConfig):
