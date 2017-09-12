b30305c326ec15c92abbaa2d3744a63166163c2b
Gabriel Saldanha <gabrielcrsaldanha@gmail.com>
2017-09-12 08:10:05 -0300

    :rocket: Remove all tables and create de default from cookiecutter

--- migrations/env.py
+++ migrations/env.py
@@ -57,7 +57,7 @@ def run_migrations_online():
 
     # this callback is used to prevent an auto-migration from being generated
     # when there are no changes to the schema
-    # reference: http://alembic.readthedocs.org/en/latest/cookbook.html
+    # reference: http://alembic.zzzcomputing.com/en/latest/cookbook.html
     def process_revision_directives(context, revision, directives):
         if getattr(config.cmd_opts, 'autogenerate', False):
             script = directives[0]
@@ -65,25 +65,21 @@ def run_migrations_online():
                 directives[:] = []
                 logger.info('No changes in schema detected.')
 
-    engine = engine_from_config(
-        config.get_section(config.config_ini_section),
-        prefix='sqlalchemy.',
-        poolclass=pool.NullPool)
+    engine = engine_from_config(config.get_section(config.config_ini_section),
+                                prefix='sqlalchemy.',
+                                poolclass=pool.NullPool)
 
     connection = engine.connect()
-    context.configure(
-        connection=connection,
-        compare_type=True,
-        target_metadata=target_metadata,
-        process_revision_directives=process_revision_directives,
-        **current_app.extensions['migrate'].configure_args)
+    context.configure(connection=connection,
+                      target_metadata=target_metadata,
+                      process_revision_directives=process_revision_directives,
+                      **current_app.extensions['migrate'].configure_args)
 
     try:
         with context.begin_transaction():
             context.run_migrations()
     finally:
         connection.close()
-
 
 if context.is_offline_mode():
     run_migrations_offline()
