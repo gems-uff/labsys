b6ed22172e41aa37ab06df6ec606a7713f226d75
Gabriel Saldanha <gabrielcrsaldanha@gmail.com>
2017-09-12 07:17:56 -0300

    :bug: Comment out  variable

--- labsys/settings.py
+++ labsys/settings.py
@@ -33,7 +33,7 @@ class DevConfig(Config):
     DEBUG = True
     # DB_NAME = 'dev.db'
     # Put the db file in project root
-    DB_PATH = os.path.join(Config.PROJECT_ROOT, DB_NAME)
+    #DB_PATH = os.path.join(Config.PROJECT_ROOT, DB_NAME)
     SQLALCHEMY_DATABASE_URI = os.environ.get('DEV_DATABASE_URL')
     DEBUG_TB_ENABLED = True
     CACHE_TYPE = 'simple'  # Can be "memcached", "redis", etc.
