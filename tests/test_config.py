2970f7b866fdf3a6db6bb0f92f0c14780a58a4e4
Gabriel Saldanha <gabrielcrsaldanha@gmail.com>
2017-09-28 00:55:02 -0300

    :hammer: Add test for testing config

--- tests/test_config.py
+++ tests/test_config.py
@@ -1,3 +1,5 @@
+import os
+
 from labsys.app import create_app
 
 
@@ -8,3 +10,7 @@ def test_production_config():
 def test_development_config():
     app = create_app('development')
     assert app.config['DEBUG'] is True
+
+def test_testing_config():
+    app = create_app('testing')
+    assert app.config['TESTING'] is True
