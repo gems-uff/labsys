cf034a6d0280dff90cdcaafe3e773171bbd75b0b
Gabriel Saldanha <gabrielcrsaldanha@gmail.com>
2017-10-16 22:27:15 -0200

    :hammer: Add more tests. But issue on subproduct

--- tests/test_inventory/test_forms.py
+++ tests/test_inventory/test_forms.py
@@ -24,12 +24,13 @@ class TestProductForm:
         assert len(form.errors) == 1
         assert error_msg in form.catalog.errors
 
-    def test_create_product_same_catalog_but_differente_manufacturer(self, db):
+    def test_valid_product_same_catalog_different_manufacturer(self, db):
         registered_product = ProductFactory(manufacturer='A', catalog='123')
         form = ProductForm(obj=registered_product)
         form.manufacturer.data = 'A different manufacturer'
-        assert form.validate() is True
+        assert form.validate() is True # this should be enough
         product_from_form = Product()
+        # TODO: move this logic to functional tests
         form.populate_obj(product_from_form)
         db.session.add(product_from_form)
         db.session.commit()
@@ -37,18 +38,54 @@ class TestProductForm:
         assert p is not None
         assert p.manufacturer == 'A different manufacturer'
 
+    def test_validation_subproduct_catalog_is_null_or_empty(self):
+        # Empty
+        non_unitary_product_form = ProductForm(
+            name='Non Unitary', manufacturer='Any Manufacturer', catalog='123',
+            stock_unit=1000, min_stock=1, subproduct_catalog=''
+        )
+        error_msg = 'Subproduto deve ser preenchido ' \
+                    'para produtos não unitários.'
+        assert non_unitary_product_form.validate() is False
+        assert error_msg in non_unitary_product_form.subproduct_catalog.errors
+        # None
+        non_unitary_product_form = ProductForm(
+            name='Non Unitary', manufacturer='Any Manufacturer', catalog='123',
+            stock_unit=1000, min_stock=1, subproduct_catalog=None
+        )
+        error_msg = 'Subproduto deve ser preenchido ' \
+                    'para produtos não unitários.'
+        assert non_unitary_product_form.validate() is False
+        assert error_msg in non_unitary_product_form.subproduct_catalog.errors
 
-    @pytest.mark.skip()
-    def test_create_unitary_product_with_correct_data(self):
-        assert False
+    def test_unitary_product_with_subproduct_input(self):
+        form = ProductForm(
+            name='Non Unitary', manufacturer='Any Manufacturer', catalog='123',
+            stock_unit=1, min_stock=1, subproduct_catalog='123'
+        )
+        assert form.validate() is False
+        error_msg = 'Produtos unitários não possuem subproduto.'
+        assert error_msg in form.subproduct_catalog.errors
 
-    @pytest.mark.skip()
-    def test_stock_unit_is_null_for_created_non_unitary_product(self):
-        assert False
+    def test_valid_non_unitary_product_with_subproduct(self):
+        subproduct = ProductFactory(catalog='321')
+        form = ProductForm(
+            name='Non Unitary', manufacturer='Any Manufacturer', catalog='123',
+            stock_unit=4, min_stock=1, subproduct_catalog='321'
+        )
+        assert form.validate() is True
 
+    # Could not test this, validate_subproduct_catalog not being called
+    # although it is called via the application
     @pytest.mark.skip()
-    def test_validate_subproduct_catalog_does_not_exist(self):
-        assert False
+    def test_subproduct_does_not_exist(self, db):
+        form = ProductForm(
+            name='Non Unitary', manufacturer='Any Manufacturer', catalog='abc',
+            stock_unit=2, min_stock=2, subproduct_catalog='99999'
+        )
+        assert form.validate() is False
+        error_msg = 'Subproduto informado não existe.'
+        assert error_msg in form.subproduct_catalog.errors
 
     @pytest.mark.skip()
     def test_create_product_linked_to_subproduct(self):
