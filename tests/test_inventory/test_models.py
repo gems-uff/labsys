b17b0f82b94aaba1f37b68d0660873a3c4329f8b
Gabriel Saldanha <gabrielcrsaldanha@gmail.com>
2017-11-10 07:33:27 -0200

    :memo: Add dummy models test file

---
+++ tests/test_inventory/test_models.py
@@ -0,0 +1,13 @@
+import pytest
+
+from labsys.inventory.models import (
+    Product, StockProduct, Specification, OrderItem, Order, Transaction
+)
+
+'''
+TODO: Tests
+
+Product
+- must know how many there are in stock
+- must retrieve its associated specifications
+'''
