c637d6f4bec06968b4cd9975a6b28402721dc8f7
Gabriel Saldanha <gabrielcrsaldanha@gmail.com>
2018-03-27 10:17:53 -0300

    :hammer: First test setup

---
+++ test/inventory/test_views.py
@@ -0,0 +1,24 @@
+import os, unittest
+import labsys
+from labsys.app import create_app, db
+from labsys.auth.models import Role
+import labsys.inventory.models as models
+
+
+class ShowCatalogTestCase(unittest.TestCase):
+
+    def setUp(self):
+        self.app = create_app('testing')
+        self.app_context = self.app.app_context()
+        self.app_context.push()
+        db.create_all()
+        Role.insert_roles()
+        self.client = self.app.test_client(use_cookies=True)
+
+    def tearDown(self):
+        db.session.remove()
+        db.drop_all()
+        self.app_context.pop()
+
+    def test_dummy(self):
+        self.assertTrue(True)
