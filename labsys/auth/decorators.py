276111f633b6151368eb38f01b222567c5ebed97
Gabriel Saldanha <gabrielcrsaldanha@gmail.com>
2018-03-27 11:01:46 -0300

    :rocket: Verify if it's a testing app for permissioning

--- labsys/auth/decorators.py
+++ labsys/auth/decorators.py
@@ -1,5 +1,5 @@
 from functools import wraps
-from flask import abort
+from flask import abort, current_app
 from flask_login import current_user
 from labsys.auth.models import Permission
 
@@ -8,7 +8,8 @@ def permission_required(permission):
     def decorator(f):
         @wraps(f)
         def decorated_function(*args, **kwargs):
-            if not current_user.can(permission):
+            if not current_user.can(permission) and \
+                not current_app.config['TESTING'] == True:
                 abort(403)
             return f(*args, **kwargs)
         return decorated_function
