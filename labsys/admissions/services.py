410b9b0161965286038681d8faf43b6a093af948
Gabriel Saldanha <gabrielcrsaldanha@gmail.com>
2018-04-10 10:47:31 -0300

    :fire: HUGE refactoring

---
+++ labsys/admissions/services.py
@@ -0,0 +1,39 @@
+from labsys.extensions import db
+
+
+def get_admission_symptoms(admission_id):
+    query = 'SELECT s.id, s.name, s.primary, obs.observed, obs.details \
+        FROM symptoms s \
+        LEFT JOIN observed_symptoms obs \
+            ON s.id = obs.symptom_id \
+        WHERE obs.admission_id = %d OR obs.admission_id IS NULL' % admission_id
+    result = db.engine.execute(query)
+
+    mapped_symptoms = [
+        {
+            'symptom_id': symptom[0],
+            'symptom_name': symptom[1],
+            'primary': symptom[2],
+            'observed': symptom[3],
+            'details': symptom[4],
+        } for symptom in result.fetchall()]
+    return mapped_symptoms
+
+
+def get_admission_risk_factors(admission_id):
+    query = 'SELECT rf.id, rf.name, rf.primary, obs.observed, obs.details \
+        FROM risk_factors rf \
+        LEFT JOIN observed_risk_factors obs \
+            ON rf.id = obs.symptom_id \
+        WHERE obs.admission_id = %d OR obs.admission_id IS NULL' % admission_id
+    result = db.engine.execute(query)
+
+    mapped_risk_factors = [
+        {
+            'risk_factor_id': risk_factor[0],
+            'risk_factor_name': risk_factor[1],
+            'primary': risk_factor[2],
+            'observed': risk_factor[3],
+            'details': risk_factor[4],
+        } for risk_factor in result.fetchall()]
+    return mapped_risk_factors
--- labsys/admissions/views.py
+++ labsys/admissions/views.py
@@ -1,8 +1,9 @@
-from flask import Blueprint, abort, flash, redirect, render_template, url_for
+from flask import abort, flash, redirect, render_template, url_for
 from flask_login import login_required
 
 from labsys.auth.decorators import permission_required
 from labsys.auth.models import Permission
+from labsys.utils.decorators import paginated
 
 from . import blueprint
 from ..extensions import db
@@ -10,25 +11,99 @@ from .forms import AdmissionForm
 from .models import (Address, Admission, CdcExam, ClinicalEvolution,
                      Hospitalization, ObservedSymptom, Patient, Sample,
                      Symptom, UTIHospitalization, Vaccine)
+from .services import get_admission_risk_factors, get_admission_symptoms
 
 IGNORED = 9
 TRUE = 1
 FALSE = 0
 
 
+def associate_dated_events_to_admission(
+        admission,
+        vaccine,
+        hospitalization,
+        uti_hospitalization,
+        clinical_evolution):
+    # Vaccine
+    occurred = vaccine.data['occurred']
+    occurred = bool(occurred) if occurred is not IGNORED else None
+    admission.vaccine = Vaccine(
+        occurred=occurred,
+        date=vaccine.data['date'],)
+    # Hospitalization
+    occurred = hospitalization.data['occurred']
+    occurred = bool(occurred) if occurred is not IGNORED else None
+    admission.hospitalization = Hospitalization(
+        occurred=occurred,
+        date=hospitalization['date'],)
+    # UTIHospitalization
+    occurred = uti_hospitalization.data['occurred']
+    occurred = bool(occurred) if occurred is not IGNORED else None
+    admission.uti_hospitalization = UTIHospitalization(
+        occurred=occurred,
+        date=uti_hospitalization['date'],)
+    # ClinincalEvolution
+    occurred = clinical_evolution.data['occurred']
+    occurred = bool(occurred) if occurred is not IGNORED else None
+    admission.clinical_evolution = ClinicalEvolution(
+        occurred=occurred,
+        date=clinical_evolution['date'],)
+
+
+def associate_observed_symptoms_to_admission(
+        admission, symptom_forms, sec_symptom_forms):
+    for symptom_form in symptom_forms:
+        if symptom_form.data['observed'] is not IGNORED:
+            ObservedSymptom(
+                observed=bool(symptom_form.data['observed']),
+                details=symptom_form.data['details'],
+                symptom_id=symptom_form.data['symptom_id'],
+                admission=admission, )
+
+    for sec_symptom_form in sec_symptom_forms:
+        if sec_symptom_form.data['observed'] is True:
+            ObservedSymptom(
+                observed=True,
+                details=sec_symptom_form.data['details'],
+                symptom_id=sec_symptom_form.data['symptom_id'],
+                admission=admission, )
+
+def associate_samples_to_admission(admission, sample_forms):
+    for sample_form in sample_forms:
+        sample = Sample(
+            admission_date=sample_form.data['admission_date'],
+            collection_date=sample_form.data['collection_date'],
+            semepi=sample_form.data['semepi'],
+            details=sample_form.data['details'],
+            method_id=sample_form.data['method_id'] if sample_form.data['method_id'] is not -1 else None,
+            admission=admission, )
+        CdcExam(
+            flu_type=sample_form.cdc_exam.data['flu_type'],
+            flu_subtype=sample_form.cdc_exam.data['flu_subtype'],
+            dominant_ct=sample_form.cdc_exam.data['dominant_ct'],
+            details=sample_form.cdc_exam.data['details'],
+            sample=sample, )
+
+
 # TODO: do I need this?
 @blueprint.app_context_processor
 def inject_permissions():
-    """This function is executed each request,
-    even though outside of the bluprint"""
+    '''This function is executed each request,
+    even though outside of the bluprint'''
     return dict(Permission=Permission)
 
 
 @blueprint.route('/', methods=['GET'])
 @permission_required(Permission.VIEW)
 def list_admissions():
-    admissions = Admission.query.all()
-    return render_template('admissions/list-admissions.html', admissions=admissions)
+    template = 'admissions/list-admissions.html'
+    view = 'admissions.list_admissions'
+    query = Admission.query.order_by(Admission.id_lvrs_intern)
+    context_title = 'admissions'
+    return paginated(query=query,
+                     template_name=template,
+                     view_method=view,
+                     context_title=context_title)
 
 
 def symptom_in_admission_symptoms(symptom_id, admission):
@@ -39,29 +114,11 @@ def symptom_in_admission_symptoms(symptom_id, admission):
     return found
 
 
-def get_symptoms(admission_id):
-    query = 'SELECT s.id, s.name, s.primary, obs.observed, obs.details \
-        FROM symptoms s \
-        LEFT JOIN observed_symptoms obs \
-            ON s.id = obs.symptom_id \
-        WHERE obs.admission_id = %d OR obs.admission_id IS NULL' % admission_id
-    result = db.engine.execute(query)
-
-    mapped_symptoms = [
-        {
-            'symptom_id': symptom[0],
-            'symptom_name': symptom[1],
-            'primary': symptom[2],
-            'observed': symptom[3],
-            'details': symptom[4],
-        } for symptom in result.fetchall()]
-    return mapped_symptoms
-
 @blueprint.route('/<int:id>', methods=['GET', 'POST'])
 @permission_required(Permission.VIEW)
 def detail_admission(admission_id):
     admission = Admission.query.get_or_404(admission_id)
-    symptoms = get_symptoms(admission_id)
+    symptoms = get_admission_symptoms(admission_id)
     primary_symptoms = []
     sec_symptoms = []
     for symptom in symptoms:
@@ -95,25 +152,14 @@ def detail_admission(admission_id):
 @permission_required(Permission.EDIT)
 def edit_admission(id):
     admission = Admission.query.get_or_404(id)
-
-    symptoms = [{
-        'symptom_id': s.id,
-        'symptom_name': s.name
-    } for s in Symptom.get_primary()]
-    sec_symptoms = [{
-        'symptom_id': s.id,
-        'symptom_name': s.name
-    } for s in Symptom.get_secondary()]
-
-    for obs_symptom in admission.symptoms:
-        for symptom in symptoms:
-            if symptom['symptom_id'] == obs_symptom.symptom.id:
-                symptom['observed'] = obs_symptom.observed
-                symptom['details'] = obs_symptom.details
-        for sec_symptom in sec_symptoms:
-            if sec_symptom['symptom_id'] == obs_symptom.symptom.id:
-                sec_symptom['observed'] = obs_symptom.observed
-                sec_symptom['details'] = obs_symptom.details
+    symptoms = get_admission_symptoms(admission_id)
+    primary_symptoms = []
+    sec_symptoms = []
+    for symptom in symptoms:
+        if symptom.primary:
+            primary_symptoms.append(symptom)
+        else:
+            sec_symptoms.append(symptom)
 
     form = AdmissionForm(
         id_lvrs_intern=admission.id_lvrs_intern,
@@ -129,7 +175,7 @@ def edit_admission(id):
         hospitalization=admission.hospitalization,
         uti_hospitalization=admission.uti_hospitalization,
         clinical_evolution=admission.clinical_evolution,
-        symptoms=symptoms,
+        symptoms=primary_symptoms,
         sec_symptoms=sec_symptoms,
         samples=admission.samples, )
     form.submit.label.text = 'Editar'
@@ -292,99 +338,49 @@ def create_admission():
     if form.validate_on_submit():
         admission = Admission.query.filter_by(
             id_lvrs_intern=form.id_lvrs_intern.data).first()
-        if admission is None:
+        if admission is not None:
+            flash('Número Interno já cadastrado! Escolha outro!', 'danger')
+        else:
             patient = Patient(
                 name=form.patient.data['name'],
                 birth_date=form.patient.data['birth_date'],
                 age=form.patient.data['age'],
                 age_unit=form.patient.data['age_unit'],
                 gender=form.patient.data['gender'], )
-
-            Address(
-                patient=patient,
-                country=form.patient.residence.data['country'] if
-                form.patient.residence.data['country'] is not -1 else None,
-                state=form.patient.residence.data['state']
-                if form.patient.residence.data['state'] is not -1 else None,
-                city=form.patient.residence.data['city']
-                if form.patient.residence.data['city'] is not -1 else None,
+            patient.residence = Address(
+                country=form.patient.residence.data['country'],
+                state=form.patient.residence.data['state'],
+                city=form.patient.residence.data['city'],
                 neighborhood=form.patient.residence.data['neighborhood'],
                 zone=form.patient.residence.data['zone'],
                 details=form.patient.residence.data['details'])
-
             admission = Admission(
                 id_lvrs_intern=form.id_lvrs_intern.data,
                 first_symptoms_date=form.first_symptoms_date.data,
                 semepi_symptom=form.semepi_symptom.data,
-                state=form.state.data
-                if form.state.data is not -1 else None,
-                city=form.city.data
-                if form.city.data is not -1 else None,
+                state=form.state.data,
+                city=form.city.data,
                 health_unit=form.health_unit.data,
                 requesting_institution=form.requesting_institution.data,
-                details=form.details.data,
-                patient=patient, )
+                details=form.details.data)
+            admission.patient = patient
 
-            if form.vaccine.data['applied'] is not IGNORED:
-                Vaccine(
-                    applied=bool(form.vaccine.data['applied']),
-                    last_dose_date=form.vaccine.data['last_dose_date'],
-                    admission=admission, )
+            associate_dated_events_to_admission(
+                admission,
+                form.vaccine,
+                form.hospitalization,
+                form.uti_hospitalization,
+                form.clinical_evolution)
 
-            if form.hospitalization.data['occurred'] is not IGNORED:
-                Hospitalization(
-                    occurred=bool(form.hospitalization.data['occurred']),
-                    date=form.hospitalization.data['date'],
-                    admission=admission, )
+            associate_observed_symptoms_to_admission(
+                admission,
+                form.symptoms,
+                form.sec_symptoms)
 
-            if form.uti_hospitalization.data['occurred'] is not IGNORED:
-                UTIHospitalization(
-                    occurred=bool(form.uti_hospitalization.data['occurred']),
-                    date=form.uti_hospitalization.data['date'],
-                    admission=admission, )
-
-            if form.clinical_evolution.data['death'] is not IGNORED:
-                ClinicalEvolution(
-                    death=bool(form.clinical_evolution.data['death']),
-                    date=form.vaccine.data['date'],
-                    admission=admission, )
-
-            for symptom_form in form.symptoms:
-                if symptom_form.data['observed'] is not IGNORED:
-                    ObservedSymptom(
-                        observed=bool(symptom_form.data['observed']),
-                        details=symptom_form.data['details'],
-                        symptom_id=symptom_form.data['symptom_id'],
-                        admission=admission, )
-
-            for sec_symptom_form in form.sec_symptoms:
-                if sec_symptom_form.data['observed'] is True:
-                    ObservedSymptom(
-                        observed=True,
-                        details=sec_symptom_form.data['details'],
-                        symptom_id=sec_symptom_form.data['symptom_id'],
-                        admission=admission, )
-
-            for sample_form in form.samples:
-                sample = Sample(
-                    admission_date=sample_form.data['admission_date'],
-                    collection_date=sample_form.data['collection_date'],
-                    semepi=sample_form.data['semepi'],
-                    details=sample_form.data['details'],
-                    method_id=sample_form.data['method_id']
-                    if sample_form.data['method_id'] is not -1 else None,
-                    admission=admission, )
-                CdcExam(
-                    flu_type=sample_form.cdc_exam.data['flu_type'],
-                    flu_subtype=sample_form.cdc_exam.data['flu_subtype'],
-                    dominant_ct=sample_form.cdc_exam.data['dominant_ct'],
-                    details=sample_form.cdc_exam.data['details'],
-                    sample=sample, )
+            associate_samples_to_admission(admission, form.samples)
 
             db.session.add(admission)
             flash('Admissão criada com sucesso!', 'success')
-        else:
-            flash('Número Interno já cadastrado! Escolha outro!', 'danger')
         return redirect(url_for('.create_admission'))
 
     return render_template(
