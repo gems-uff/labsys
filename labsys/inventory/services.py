ee19141e33ca444d849f18a500d7845d81046946
Gabriel Saldanha <gabrielcrsaldanha@gmail.com>
2018-09-19 09:51:08 -0300

    Add lot_number to transactions

--- labsys/inventory/services.py
+++ labsys/inventory/services.py
@@ -2,13 +2,12 @@ from ..extensions import db
 from labsys.inventory import models as m
 
 
-def create_add_transaction_from_order(order, stock):
+def create_add_transactions_from_order(order, stock):
     user = order.user
     for order_item in order.items:
         product = order_item.item.product
         lot_number = order_item.lot_number
         total_units = order_item.amount * order_item.item.units
-        expiration_date = order_item.expiration_date
         transaction = m.Transaction(
             user,
             product,
@@ -16,7 +15,6 @@ def create_add_transaction_from_order(order, stock):
             total_units,
             stock,
             m.ADD,
-            expiration_date,
         )
         db.session.add(transaction)
     db.session.commit()
